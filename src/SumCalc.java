b25586faa042441ae8b934e3ad29808a10953d80
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:53:33 -0800

    Initial commit

---
+++ src/SumCalc.java
@@ -0,0 +1,11 @@
+public class SumCalc{
+    public static void main(String[] args){
+        int a, b, c, d;
+        a=5;
+        b=10;
+        c=15;
+        d=20;
+        int sum = a+b+c+d;
+        System.out.println(sum);
+    }
+}
